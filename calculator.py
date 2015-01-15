b2c383a476690f4b44c8c4cca02936268e3cf658
Hackbright Student <info@hackbrightacademy.com>
2015-01-15 13:04:28 -0800

    CALCULATOR WORKS

--- calculator.py
+++ calculator.py
@@ -7,14 +7,44 @@ calculator program yourself in this file.
 from arithmetic import *
 
 def main():
-    input_num = raw_input("> ")
-    math_tokens = input_num.split(" ")
-    arith = math_tokens[0]
-    num1 = int(math_tokens[1])
-    num2 = int(math_tokens[2])
-    if arith == "+":
-        print add(num1, num2)
-    return
+    while True:
+        input_num = raw_input("> ")
+        math_tokens = input_num.split(" ")
+        token_cnt = len(math_tokens)
+        if token_cnt == 3:
+            arith = math_tokens[0]
+            numb1 = int(math_tokens[1])
+            numb2 = int(math_tokens[2])
+            if arith == "+":
+                print add(numb1, numb2)
+                continue
+            elif arith == "-":
+                print subtract(numb1, numb2)
+                continue
+            elif arith == "*":
+                print multiply(numb1, numb2)
+                continue
+            elif arith == "/":
+                print divide(numb1, numb2)
+                continue
+            elif arith == "pow":
+                print power(numb1, numb2)
+                continue
+            elif arith == "mod":
+                print mod(numb1, numb2)
+                continue
+        if token_cnt == 2:
+            arith = math_tokens[0]
+            numb1 = int(math_tokens[1])
+            if arith == "square":
+                print square(numb1)
+                continue
+            elif arith == "cube":
+                print cube(numb1)
+                continue
+        if math_tokens[0] == "q":
+            break
+ 
 
 
 if __name__ == '__main__':

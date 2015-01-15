8d408cac4d9918fb4452bc434d6afa97e5f78733
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 18:13:42 -0800

    FIXED CALC NAILED IT

--- calculator.py
+++ calculator.py
@@ -7,13 +7,13 @@ calculator program yourself in this file.
 from arithmetic import *
 
 def main():
-    input = raw_input("> ")
-    math_tokens = input.split(" ")
+    input_num = raw_input("> ")
+    math_tokens = input_num.split(" ")
     arith = math_tokens[0]
     num1 = int(math_tokens[1])
     num2 = int(math_tokens[2])
     if arith == "+":
-        add(num1, num2)
+        print add(num1, num2)
     return
 
 
